a1f6a2ef7b636f3acfb8934d40f2cff4aabe11a5
Candelario-Hudson <magic.mario2021@gmail.com>
2023-07-27 17:02:36 -0500

    Partially completed Computrabajo Pipeline

--- mining/main.py
+++ mining/main.py
@@ -6,15 +6,46 @@ import os
 os.environ["PATH"] += r"C:/SeleniumDrivers"
 
 
-def get_job(key_word: str, location: str = 'Colombia'):
-    browser.get(f"https://co.computrabajo.com/trabajo-de-{key_word}-en-{location}")
-    browser.implicitly_wait(30)
-    return browser.find_elements(By.CLASS_NAME, "box_offer")
+def get_job(key_word: str, location: str = "Colombia", pag_count: int = 5):
+    """
+    Obtiene datos de ofertas de trabajo de Computrabajo para una palabra clave y ubicación específicas.
+
+    Parámetros:
+        - key_word (str): Palabra clave para la búsqueda de trabajo.
+        - location (str, opcional): Ubicación para la búsqueda de trabajo. Por defecto es "Colombia".
+        - pag_count (int, opcional): Número de páginas a obtener. Por defecto es 5.
+
+    Retorna:
+        Una lista de diccionarios con los datos de las ofertas de trabajo scrapeadas de Computrabajo.
+    """
+    scraped_data = []  # Inicializar una lista vacía para almacenar los datos scrapeados
+
+    for page_num in range(start=1, stop=pag_count):
+        url = f"https://co.computrabajo.com/trabajo-de-{key_word}-en-{location}?p={page_num}"
+        browser.get(url)
+        browser.implicitly_wait(30)
+        app = browser.find_elements(By.CLASS_NAME, "box_offer")
+
+        # Llamar a la función computrabajo_scraper para obtener datos de la página actual
+        page_data = computrabajo_scraper(app)
+        scraped_data.extend(
+            page_data
+        )  # Agregar los datos de la página actual a la lista general
+
+    return scraped_data
 
 
 def computrabajo_scraper(app: list):  # sourcery skip: use-contextlib-suppress
-    scraped_data = []  # Inicializar una lista vacía para almacenar los datos scrapeados
+    """
+    Obtiene datos de ofertas de trabajo de la página de Computrabajo.
 
+    Parámetros:
+        - app (list): Lista de elementos web que contienen información sobre las ofertas de trabajo.
+
+    Retorna:
+        Una lista de diccionarios con los datos de las ofertas de trabajo scrapeadas de la página.
+    """
+    scraped_data = []  # Inicializar una lista vacía para almacenar los datos scrapeados
     for item in app:
         title = item.find_element(By.TAG_NAME, "h2").text
         link_element = browser.find_element(By.CLASS_NAME, "js-o-link")
@@ -42,7 +73,7 @@ def computrabajo_scraper(app: list):  # sourcery skip: use-contextlib-suppress
         # Agregar el diccionario de datos a la lista
         scraped_data.append(
             {
-                'platform': 'Computrabajo',
+                "platform": "Computrabajo",
                 "title": title,
                 "company": company,
                 "location": location_output,
@@ -55,6 +86,6 @@ def computrabajo_scraper(app: list):  # sourcery skip: use-contextlib-suppress
 
 if __name__ == "__main__":
     browser = webdriver.Chrome()
-    app = get_job(key_word="python")
-    result = computrabajo_scraper(app=app)
+    result = get_job(key_word="python")
     print(result)
+    print(len(result))
